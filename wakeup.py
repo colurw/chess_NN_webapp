7a1396bdbf16c103a64c261019249d99d3f8a307
colurw <colurw@gmail.com>
2024-02-09 16:09:38 +0000

    wakeup fixed

--- wakeup.py
+++ wakeup.py
@@ -3,14 +3,10 @@
     delay to the first user of the day.  Wakeup.py prevents this."""
 
 from pythonping import ping
+import urllib3
 import time
 
-target = 'http://www.ec2-18-169-205-217.eu-west-2.compute.amazonaws.com/play'
+target = "http://ec2-18-169-205-217.eu-west-2.compute.amazonaws.com/play/"
 
-while True:
-    print('pinging server now')
-    try:
-        ping(target)
-        time.sleep(300)
-    except:
-        time.sleep(300)
+time.sleep(120)
+urllib3.request("GET", target)
